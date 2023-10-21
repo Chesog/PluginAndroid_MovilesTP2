280f3ed0870f7c226d6f710aeb2cc03735f88c57
Dario <otrantodario@gmail.com>
2023-10-20 21:58:19 -0300

    Add Send Function for the unity logs

--- AndroidStudio/otranto_loger_plugin/src/main/java/com/otranto_loger_plugin/Otranto_Logger.java
+++ AndroidStudio/otranto_loger_plugin/src/main/java/com/otranto_loger_plugin/Otranto_Logger.java
@@ -1,8 +1,13 @@
 package com.otranto_loger_plugin;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Otranto_Logger
 {
     static final String LOGTAG = "OTRLGG";
+    List<String> warnings = new ArrayList<>();
+    List <String> errors = new ArrayList<>();
+    List <String> debug = new ArrayList<>();
     static Otranto_Logger _instance = null;
 
     public static Otranto_Logger GetInstance()
@@ -10,6 +15,21 @@ public class Otranto_Logger
         if (_instance == null)
             _instance = new Otranto_Logger();
         return _instance;
+    }
+
+    public void SendLog(String log)
+    {
+        this.debug.add(log);
+    }
+
+    public void SendWarning(String warning)
+    {
+        this.warnings.add(warning);
+    }
+
+    public void SendError(String error)
+    {
+        this.errors.add(error);
     }
 
     public String GetLOGTAG()
