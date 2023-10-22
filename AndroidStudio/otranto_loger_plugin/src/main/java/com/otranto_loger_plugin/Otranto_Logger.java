62466a126be49949418fcbf89051e22490ff8e3a
Dario <otrantodario@gmail.com>
2023-10-22 01:18:58 -0300

    Add writeToFile and readFromFile Functions

--- AndroidStudio/otranto_loger_plugin/src/main/java/com/otranto_loger_plugin/Otranto_Logger.java
+++ AndroidStudio/otranto_loger_plugin/src/main/java/com/otranto_loger_plugin/Otranto_Logger.java
@@ -2,9 +2,16 @@ package com.otranto_loger_plugin;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -82,5 +89,46 @@ public class Otranto_Logger
     public String GetLOGTAG()
     {
         return LOGTAG;
+    }
+
+    private void writeToFile(String data,String fileName, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileName + ".txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile(String fileName,Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(fileName + ".txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 }
